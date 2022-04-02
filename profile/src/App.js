ede62936f84e9d043b494a09b171581e823b3ea8
고윤혁 <sksgur3217@gmail.com>
2022-04-02 15:11:48 +0900

    second commit

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
+// css
 import './App.css';
+import { gsap } from 'gsap';
+// components
+import Skill from './components/Skill';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div className="App">profile</div>
-      <div className="App">profile</div>
-      <div className="App">profile</div>
-      <div className="App">profile</div>
-    </>
+    <div className="App">
+      <header className="header">
+        <div>header</div>
+      </header>
+      <main className="main">
+        <Skill />
+      </main>
+      <footer className="footer">footer</footer>
+    </div>
   );
-}
+};
 
 export default App;
+
+/*
+flip, fade-in 등의 애니메이션 : AOS
+복잡한 애니메이션 구현 : GSAP
+별 내리는 애니메이션 샘플 코드 : Star animation
+색상 조합 사이트 : Color hunt
+무료 아이콘 사이트 : Iconmonstr
+*/
---
+++ profile/src/components/Skill.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// css
+import './Skill.css';
+
+const Skill = () => {
+  return (
+    <div className="Skills">
+      <div>Skills</div>
+    </div>
+  );
+};
+
+export default Skill;
