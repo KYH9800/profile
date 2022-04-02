0865cd7ddc9342d861f7c6f7a403eebe87741278
고윤혁 <sksgur3217@gmail.com>
2022-04-02 13:42:05 +0900

    setting myPortfolio

---
+++ profile/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">profile</div>
+      <div className="App">profile</div>
+      <div className="App">profile</div>
+      <div className="App">profile</div>
+    </>
+  );
+}
+
+export default App;
