eb4ec58a552a9e6b70f6f3df8c31a6d6a3c1d9ec
고윤혁 <sksgur3217@gmail.com>
2022-04-07 18:25:06 +0900

    contact line ...ing

--- profile/src/components/Contact.js
+++ profile/src/components/Contact.js
@@ -1,11 +1,49 @@
 import React from 'react';
 // css
 import './Contact.css';
+import { PhoneOutlined, MailOutlined, InstagramOutlined, BoldOutlined, GithubOutlined } from '@ant-design/icons';
 
 const Contact = () => {
   return (
     <div className="contactWrapper">
-      <div>Contact</div>
+      <main>
+        <h1>Contact</h1>
+        <h2>
+          Yun Hyeok Ko <span className="years">@2022</span>
+        </h2>
+        <div className="intro-wrapper">
+          <p>
+            <PhoneOutlined />
+            <a href="" target="blank">
+              010-9800-5226
+            </a>
+          </p>
+          <p>
+            <MailOutlined />
+            <a href="" target="blank">
+              sksgur3217@gmail.com
+            </a>
+          </p>
+          <p>
+            <GithubOutlined />
+            <a href="" target="blank">
+              github
+            </a>
+          </p>
+          <p>
+            <BoldOutlined />
+            <a href="" target="blank">
+              Blog
+            </a>
+          </p>
+          <p>
+            <InstagramOutlined />
+            <a href="" target="blank">
+              Instagram
+            </a>
+          </p>
+        </div>
+      </main>
     </div>
   );
 };
